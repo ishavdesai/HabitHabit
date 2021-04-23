c6037fba807b4cd07661f20e55d4e904a84b6148
Ally <user@Allys-MacBook-Air.local>
2021-04-23 00:39:42 -0500

    no change

--- HabitHabit/UIDesign.swift
+++ HabitHabit/UIDesign.swift
@@ -11,6 +11,8 @@ import UIKit
 class UIDesign {
     
     static func cleanupButton(button: UIButton!) -> Void {
+        
+        
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor(red: 132/255, green: 55/255, blue: 135/255, alpha: 1).cgColor
         button.layer.backgroundColor = UIColor(red: 132/255, green: 55/255, blue: 135/255, alpha: 0.75).cgColor
@@ -19,5 +21,7 @@ class UIDesign {
         button.layer.shadowRadius = 3
         button.layer.shadowOpacity = 0.5
         button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.frame = CGRect(x: button.layer.frame.origin.x, y: button.layer.frame.origin.y, width: 300, height: 30)
+        
     }
 }
