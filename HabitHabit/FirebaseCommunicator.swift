310c9bc52302263f96cc7c228ed14937581ed309
ishdes <31709580+ishdes@users.noreply.github.com>
2021-04-22 23:32:47 -0500

    Improved code quality, error checking

---
+++ HabitHabit/FirebaseCommunicator.swift
@@ -0,0 +1,30 @@
+//
+//  FirebaseCommunicator.swift
+//  HabitHabit
+//
+//  Created by Ishav Desai on 4/22/21.
+//
+
+import UIKit
+
+class FirebaseCommunicator {
+    
+    
+    
+    static func makeHabit(value: [String: String]) -> (Bool, Habit?) {
+        let habit: String = value["habit"] ?? "NO_HABIT_EXISTS"
+        let timeToRemind: String = value["timeToRemind"] ?? "NO_TIME_TO_REMIND"
+        let streak: Int = Int(value["streak"] ?? "") ?? -1
+        let dateString: String = value["dates"] ?? ""
+        let dates: [Date] = (dateString.count == 0) ? [] : Habit.convertStringListToDateList(strList: dateString.components(separatedBy: ","))
+        let uncheckedDateString: String = value["uncheckedDates"] ?? ""
+        let uncheckedDates: [Date] = (uncheckedDateString.count == 0) ? [] : Habit.convertStringListToDateList(strList: uncheckedDateString.components(separatedBy: ","))
+        let imageUrlsString: String = value["imageUrls"] ?? ""
+        let imageUrls: [String] = (imageUrlsString == "") ? [] : imageUrlsString.components(separatedBy: ",")
+        let uncheckedImageUrlsString: String = value["uncheckedImageUrls"] ?? ""
+        let uncheckedImageUrls: [String] = (uncheckedImageUrlsString == "") ? [] : uncheckedImageUrlsString.components(separatedBy: ",")
+        let habitExists: Bool = habit != "NO_HABIT_EXISTS" && streak != -1 && timeToRemind != "NO_TIME_TO_REMIND"
+        let habitResult: Habit? = habitExists ? Habit(habit: habit, streak: streak, dates: dates, timeToRemind: timeToRemind, imageUrls: imageUrls, uncheckedImageUrls: uncheckedImageUrls, uncheckedDates: uncheckedDates) : nil
+        return (habitExists, habitResult)
+    }
+}
--- HabitHabit/PeerTableViewController.swift
+++ HabitHabit/PeerTableViewController.swift
@@ -75,7 +75,7 @@ class PeerTableViewController: UITableViewController, DeleteFriendHabitFromTable
                     snapshotHabit in
                     for case let childHabit as DataSnapshot in snapshotHabit.children {
                         guard let habitValue = childHabit.value as? [String: String] else { return }
-                        let (habitExists, habit): (Bool, Habit?) = self.makeHabit(value: habitValue)
+                        let (habitExists, habit): (Bool, Habit?) = FirebaseCommunicator.makeHabit(value: habitValue)
                         if habitExists {
                             for index in 0..<habit!.uncheckedImageUrls.count {
                                 self.friendHabits.append(NameHabit(username: friend, habitName: habit!.habit, imageUrl: habit!.uncheckedImageUrls[index], date: habit!.uncheckedDates[index], habit: habit!))
@@ -87,23 +87,6 @@ class PeerTableViewController: UITableViewController, DeleteFriendHabitFromTable
                 }
             }
         }
-    }
-    
-    private func makeHabit(value: [String: String]) -> (Bool, Habit?) {
-        let habit: String = value["habit"] ?? "NO_HABIT_EXISTS"
-        let timeToRemind: String = value["timeToRemind"] ?? "NO_TIME_TO_REMIND"
-        let streak: Int = Int(value["streak"] ?? "") ?? -1
-        let dateString: String = value["dates"] ?? ""
-        let dates: [Date] = (dateString.count == 0) ? [] : Habit.convertStringListToDateList(strList: dateString.components(separatedBy: ","))
-        let uncheckedDateString: String = value["uncheckedDates"] ?? ""
-        let uncheckedDates: [Date] = (uncheckedDateString.count == 0) ? [] : Habit.convertStringListToDateList(strList: uncheckedDateString.components(separatedBy: ","))
-        let imageUrlsString: String = value["imageUrls"] ?? ""
-        let imageUrls: [String] = (imageUrlsString == "") ? [] : imageUrlsString.components(separatedBy: ",")
-        let uncheckedImageUrlsString: String = value["uncheckedImageUrls"] ?? ""
-        let uncheckedImageUrls: [String] = (uncheckedImageUrlsString == "") ? [] : uncheckedImageUrlsString.components(separatedBy: ",")
-        let habitExists: Bool = habit != "NO_HABIT_EXISTS" && streak != -1 && timeToRemind != "NO_TIME_TO_REMIND"
-        let habitResult: Habit? = habitExists ? Habit(habit: habit, streak: streak, dates: dates, timeToRemind: timeToRemind, imageUrls: imageUrls, uncheckedImageUrls: uncheckedImageUrls, uncheckedDates: uncheckedDates) : nil
-        return (habitExists, habitResult)
     }
     
 }
